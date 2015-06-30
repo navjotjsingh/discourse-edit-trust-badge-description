dd703b9154475996fc8738879a18ed1be02fd7af
Navjot Singh <navjotjsingh@gmail.com>
2015-06-30 17:06:16 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,6 @@
 # name: discourse-edit-trust-badge-description
 # about: Editing Default Trust Badge Descriptions
-# authors: Navjot
+# author: Navjot Singh
+# version: 0.2
+# url: https://github.com/navjotjsingh/discourse-edit-trust-badge-description
+# Supported Discourse version: v1.4
